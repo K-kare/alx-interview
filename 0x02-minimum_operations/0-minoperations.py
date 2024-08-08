3b1b75470f58b8746320b73dea4f1c3f7b050e96
K-kare <kamunawangari@gmail.com>
2024-08-08 12:38:37 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """ Module for 0-minoperations"""
-def minOperations(n):
 
-"""
-minOperations
-Gets fewest # of operations needed to result in exactly n H characters
-"""
-if n <= 1:
-return 0
-divisor = 2
-operations = 0
-while n > 1:
-# if n evenly divides by root
-if n % divisor == 0
-n = n / divisor
-operations = operations + divisor
-else:
-divisor += 1
-return operations
+
+def minOperations(n):
+    """
+    minOperations
+    Gets fewest # of operations needed to result in exactly n H characters
+    """
+    if (n < 2):
+        return 0
+    operations = 0
+    divisor = 2 
+    while n > 1:
+        # if n evenly divides by root
+        if n % divisor == 0:
+            # total even-divisions by root = total operations
+            n = n / divisor
+            operations = operations + divisor
+       else:
+           divisor += 1
+    return operations
