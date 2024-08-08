b484c68d513268caf8cb94e7e73b785585615577
K-kare <kamunawangari@gmail.com>
2024-08-08 12:18:54 +0000

    0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,51 +1,20 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 
 def minOperations(n):
-    """
-    minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
-    """
-    if n <= 1:
-        return 0
-    
-    operations = 0
-    divisor = 0
 
-    while n > 1:
-        # if n evenly divides by root
-        if n % divisor == 0:
-            # total even-divisions by root = total operations
-            n = n / divisor
-            operations = operations + divisor
-
-        else:
-             # increment root until it evenly-divides n
-            divisor += 1
-    return operations
-=======
-#!/usr/bin/python3
-
-def minOperations(n):
-    """
-    minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
-    """
-    if n <= 1:
-        return 0
-    
-    operations = 0
-    divisor = 0
-
-    while n > 1:
-        # if n evenly divides by root
-        if n % divisor == 0:
-            # total even-divisions by root = total operations
-            n = n / divisor
-            operations = operations + divisor
-
-        else:
-             # increment root until it evenly-divides n
-            divisor += 1
-    return operations
->>>>>>> f6299c2771438d50487f54afcd0e73a55b80897b
+"""
+minOperations
+Gets fewest # of operations needed to result in exactly n H characters
+"""
+if n <= 1:
+return 0
+divisor = 2
+operations = 0
+while n > 1:
+# if n evenly divides by root
+if n % divisor == 0
+n = n / divisor
+operations = operations + divisor
+else:
+divisor += 1
+return operations
--- 0x02-minimum_operations/README.md
+++ 0x02-minimum_operations/README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 0x02. Minimum Operations
-=======
-00x02. Minimum Operations
->>>>>>> f6299c2771438d50487f54afcd0e73a55b80897b
