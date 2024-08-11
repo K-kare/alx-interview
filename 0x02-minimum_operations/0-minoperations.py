925cea1291f437ef2f2efff0a75c4b10d9e495bb
K-kare <kamunawangari@gmail.com>
2024-08-11 16:53:11 +0000

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,6 +7,7 @@ def minOperations(n):
     minOperations
     Gets fewest # of operations needed to result in exactly n H characters
     """
+
     if (n < 2):
         return 0
     operations = 0;
