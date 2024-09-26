d74b7230d1e1848b87bc16551f5715f9961fb110
K-kare <kamunawangari@gmail.com>
2024-09-26 20:49:47 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,44 +1,19 @@
 #!/usr/bin/python3
-"""Island Perimeter Problem
 """
+Calculate Island Perimeter:
+    returns the perimeter of the island described in grid
+"""
+
+
 def island_perimeter(grid):
- """
-Calculates the perimeter of the island described in grid
-Return:
-the perimeter of the island
-"""
- counter = 0
-    grid_max = len(grid) - 1
-    lst_max = len(grid[0]) - 1 
-
-    for lst_idx, lst in enumerate(grid):
-        for land_idx, land in enumerate(lst):
-            if land == 1:
-                if land_idx == 0:
-                    counter += 1
-                    if lst[land_idx + 1] == 0:
-                        counter += 1
-                elif land_idx == lst_max:
-                    if lst[land_idx - 1] == 0:
-                        counter += 1
-                    counter += 1
-                else:
-                    if lst[land_idx - 1] == 0:
-                        counter += 1
-                    if lst[land_idx + 1] == 0:
-                        counter += 1
-                if lst_idx == 0:
-                    counter += 1
-                    if grid[lst_idx + 1][land_idx] == 0:
-                        counter += 1
-                elif lst_idx == grid_max:
-                    if grid[lst_idx - 1][land_idx] == 0:
-                        counter += 1
-                    counter += 1
-                else:
-                    if grid[lst_idx - 1][land_idx] == 0:
-                        counter += 1
-                    if grid[lst_idx + 1][land_idx] == 0:
-                        counter += 1
-
-    return counter
+    """island perimenter function"""
+    peri = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                peri += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    peri -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    peri -= 2
+    return peri
