896d5ceba3bacd6b79efa646fc86a05df65c0276
K-kare <kamunawangari@gmail.com>
2024-09-26 20:27:57 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,16 +7,38 @@ Calculates the perimeter of the island described in grid
 Return:
 the perimeter of the island
 """
-per = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if (grid[i][j] == 1):
-                if (i <= 0 or grid[i - 1][j] == 0):
-                    per += 1
-                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
-                    per += 1
-                if (j <= 0 or grid[i][j - 1] == 0):
-                    per += 1
-                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
-                    per += 1
-    return per
+ counter = 0
+    grid_max = len(grid) - 1
+    lst_max = len(grid[0]) - 1 
+
+    for lst_idx, lst in enumerate(grid):
+        for land_idx, land in enumerate(lst):
+            if land == 1:
+                if land_idx == 0:
+                    counter += 1
+                    if lst[land_idx + 1] == 0:
+                        counter += 1
+                elif land_idx == lst_max:
+                    if lst[land_idx - 1] == 0:
+                        counter += 1
+                    counter += 1
+                else:
+                    if lst[land_idx - 1] == 0:
+                        counter += 1
+                    if lst[land_idx + 1] == 0:
+                        counter += 1
+                if lst_idx == 0:
+                    counter += 1
+                    if grid[lst_idx + 1][land_idx] == 0:
+                        counter += 1
+                elif lst_idx == grid_max:
+                    if grid[lst_idx - 1][land_idx] == 0:
+                        counter += 1
+                    counter += 1
+                else:
+                    if grid[lst_idx - 1][land_idx] == 0:
+                        counter += 1
+                    if grid[lst_idx + 1][land_idx] == 0:
+                        counter += 1
+
+    return counter
